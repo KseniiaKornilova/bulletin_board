ce6ac413f4075f5b0b79bbde816621fad523ffb6
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-02-07 19:39:33 +0600

    feat add user profile page

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import index, other_page
+from .views import index, other_page, BBLoginView
 
 app_name = 'main'
 urlpatterns = [
     path('', index, name='index'),
     path('<str:page>/', other_page, name='other'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
-
+# Вывод главной страницы
 def index(request):
     return render(request, 'main/index.html')
 
+# Вывод страницы "о сайте"
 def other_page(request, page):
     try:
         template = get_template('main/' + page + '.html')
     except TemplateDoesNotExist:
         raise Http404
     return HttpResponse(template.render(request=request))
+
+# Реализация входа на сайт
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
