f14fe35d97b63b6d377ade2392b799876ccf8442
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-02-07 19:58:18 +0600

    feat add sign out page

--- main/views.py
+++ main/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 # Вывод главной страницы
@@ -20,3 +22,13 @@ def other_page(request, page):
 # Реализация входа на сайт
 class BBLoginView(LoginView):
     template_name = 'main/login.html'
+
+# Вывод страницы профиля пользователя
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
+
+# Реализация выхода с сайта
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
+
