c2beff942d13a730562b65176d54dc735fdf6a94
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-02-08 23:00:57 +0600

    feat add password change page

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,15 @@
 
 from django.urls import path
-from .views import index, other_page, BBLoginView, ChangeUserInfoView, profile, BBLogoutView
+from main import views
 app_name = 'main'
 urlpatterns = [
-    path('', index, name='index'),
-    path('<str:page>/', other_page, name='other'),
-    path('accounts/login/', BBLoginView.as_view(), name='login'),
-    path('account/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
-    path('accounts/profile/', profile, name='profile'), 
-    path('account/logout/', BBLogoutView.as_view(), name='logout'),
+    path('', views.index, name='index'),
+    path('<str:page>/', views.other_page, name='other'),
+    path('accounts/login/', views.BBLoginView.as_view(), name='login'),
+    path('account/profile/change/', views.ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/profile/', views.profile, name='profile'), 
+    path('account/logout/', views.BBLogoutView.as_view(), name='logout'),
+    path('account/password/change/', views.BBPasswordChangeView.as_view(), name='password_change'),
     
 ]
 
