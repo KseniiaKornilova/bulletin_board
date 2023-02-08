83866a6ed92b16357fd8e5b19ffff1503fc6eaaf
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-02-08 22:46:13 +0600

    feat change of personal data

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,14 @@
 
 from django.urls import path
-from .views import index, other_page, BBLoginView, profile, BBLogoutView
-
+from .views import index, other_page, BBLoginView, ChangeUserInfoView, profile, BBLogoutView
 app_name = 'main'
 urlpatterns = [
     path('', index, name='index'),
     path('<str:page>/', other_page, name='other'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('account/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/profile/', profile, name='profile'), 
-    path('account/logout/', BBLogoutView.as_view(), name='logout')
+    path('account/logout/', BBLogoutView.as_view(), name='logout'),
+    
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
+
+from .models import AdvUser
+from .forms import ChangeUserInfoForm
+
 
 # Create your views here.
 # Вывод главной страницы
@@ -31,4 +38,22 @@ def profile(request):
 # Реализация выхода с сайта
 class BBLogoutView(LoginRequiredMixin, LogoutView):
     template_name = 'main/logout.html'
+
+# Реализация изменения данных пользователя
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = AdvUser
+    template_name = 'main/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Данные пользователя изменены'
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
+        
 
